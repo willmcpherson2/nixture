ddcce51add7fec936273bd09d9cd0ad44d9cd953
William McPherson <willmcpherson2@gmail.com>
2023-03-31 21:45:45 +1100

    Abandon composition to make life easier

--- default.nix
+++ default.nix
@@ -38,69 +38,22 @@ rec {
         wait
       '';
 
-  compileOrg = file: outPath:
+  compileNixture = root:
     runCommand
-      "compile-org"
-      { buildInputs = [ pandoc ]; }
-      ''
-        mkdir -p "$(dirname "$out/${outPath}")"
-        pandoc -f org -t html ${file} > $out/${outPath}
-      '';
-
-  copyFile = path: file:
-    runCommand
-      "copy-file"
-      { }
-      ''
-        mkdir -p "$(dirname "$out/${path}")"
-        cp ${file} "$out/${path}"
-      '';
-
-  compileJinjaInput = root: file:
-    let
-      dir = builtins.dirOf file;
-      nameAndExtension = builtins.match "([^\.]*)\.(.*)" (baseNameOf file);
-      name = builtins.elemAt nameAndExtension 0;
-      extension = builtins.elemAt nameAndExtension 1;
-      pathFromRoot = file:
-        builtins.substring
-          (builtins.stringLength (toString root) + 1)
-          (builtins.stringLength (toString file))
-          (toString file);
-      cases = {
-        org = compileOrg file (pathFromRoot (dir + "/${name}.html"));
-      };
-      plain = copyFile (pathFromRoot file) file;
-      results =
-        if builtins.isList nameAndExtension
-          && builtins.length nameAndExtension == 2
-          && cases ? "${extension}"
-        then [ cases."${extension}" ]
-        else [ ];
-    in
-    results ++ [ plain ];
-
-  compileJinjaInputs = dir:
-    symlinkJoin {
-      name = "jinja-inputs";
-      paths =
-        builtins.concatMap
-          (compileJinjaInput dir)
-          (lib.filesystem.listFilesRecursive dir);
-    };
-
-  compileJinja = template:
-    let
-      dir = builtins.dirOf template;
-      outName = lib.strings.removeSuffix ".j2" (builtins.baseNameOf template);
-      jinja-inputs = compileJinjaInputs dir;
-    in
-    runCommand
-      "compile-jinja"
-      { buildInputs = [ j2cli ]; }
+      "compile-nixture"
+      { buildInputs = [ j2cli pandoc ]; }
       ''
         mkdir -p $out
-        cp -r ${jinja-inputs}/* .
-        j2 ${template} > $out/${outName}
+        cp -r ${root}/* $out
+        cd $out
+
+        for file in $(find . -type f -name "*.org"); do
+          pandoc -f org -t html -o "$file".html "$file" 
+        done
+
+        for file in $(find ~+ -type f -name "*.j2"); do
+          no_extension=$(echo "$file" | sed -n "s/\(.*\)\..*/\1/p")
+          j2 -o "$no_extension" "$file"
+        done
       '';
 }
